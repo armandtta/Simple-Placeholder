d5e38216bfed95380fa51a2717e2aa981d7485b2
Armandtta <tchiofouoarmand@gmail.com>
2020-01-20 16:00:03 +0100

    Correction des paramètres d'appel à la fonction: Table de multiplication d'un nombre x dans une borne donnée. [a,b]

--- exercice.php
+++ exercice.php
@@ -1,7 +1,7 @@
 // Affichez à l’écran la table de multiplication jusqu’à 12 x 12.
-<?php  function Multiplication ($n,$n)
+<?php  function Multiplication ($n,$a)
  {
-    for ($i = 0; $i <=$n; $i++)
+    for ($i = 0; $i <=$a; $i++)
        {
         echo $n. " X ".$i." = ".$n * $i . '<br />' ;
        }       
