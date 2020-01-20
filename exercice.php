2734ff8a75889f322c361ef25a22aabf14886c20
Armandtta <tchiofouoarmand@gmail.com>
2020-01-20 15:21:40 +0100

    Création de la fonction multiplication dans le fichier exercice.php et création d'un fichier texte vide.

---
+++ exercice.php
@@ -0,0 +1,10 @@
+// Affichez à l’écran la table de multiplication jusqu’à 12 x 12.
+<?php  function Multiplication ($n)
+ {
+    for ($i = 0; $i <=12; $i++)
+       {
+        echo $n. " X ".$i." = ".$n * $i . '<br />';
+       }       
+ }
+?>
+
